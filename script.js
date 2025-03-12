36302834a0107b4f74b3121e5f8ba3cee8071a1c
SurestPoem <hoth-yoda@outlook.com>
2025-03-12 23:29:39 +0000

    Fixed issues

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeText() {
     alert("Sigma Sigma on the wall, whose the skibidiest of them all");
 }
 
-function redirect(){
-    window.open = "https://youtu.be/dQw4w9WgXcQ?si=oJQ871mySUGnJNl9";
-    document.getElementById("text").innerHTML = "That was quite juvinile and Reddit ass humor mb";
+function redirect() {
+    window.open("https://youtu.be/dQw4w9WgXcQ?si=oJQ871mySUGnJNl9", "_blank");
+    document.getElementById("text").innerHTML = "That was quite juvenile and Reddit-ass humor mb";
 }
