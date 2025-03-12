3493f8d2f2ee2e4c387d72ffe9a0a41496c23b9d
SurestPoem <hoth-yoda@outlook.com>
2025-03-12 23:21:52 +0000

    Added buttons and stuff

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function changeText() {
     alert("Sigma Sigma on the wall, whose the skibidiest of them all");
 }
+
+function redirect(){
+    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=oJQ871mySUGnJNl9";
+    document.getElementById("text").innerHTML = "That was quite juvinile and Reddit ass humor mb";
+}
